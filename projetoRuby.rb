18e72132c1a5e7933281e2542a9973cd38ea75db
Vinicius Ferreira <133901100+vinsxzero@users.noreply.github.com>
2024-08-21 13:43:15 -0300

    Add files via upload

---
+++ projetoRuby.rb
@@ -0,0 +1,7 @@
+print "Digite seu nome: "
+nome = gets.chomp
+print "Digite seu sobrenome: "
+sobrenome  = gets.chomp
+print "Sua idade: "
+idade = gets.chomp
+puts"Olá #{nome} #{sobrenome}! Seja bem vindo, sua idade é #{idade}"
